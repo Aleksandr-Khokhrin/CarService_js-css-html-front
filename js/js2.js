957824a7cf207fed997d84e8a7c093bb839ca72d
Aleksandr <125996874+MrAlex-Neo@users.noreply.github.com>
2023-03-22 12:15:16 +0500

    Update js2.js

--- js/js2.js
+++ js/js2.js
@@ -1,3 +1,4 @@
+// слайдер
 let splideS = new Splide( '.splide.second', {
     type   : 'loop',
     perPage: 1,
@@ -6,14 +7,21 @@ let splideS = new Splide( '.splide.second', {
   } );
   splideS.mount();
 
-  
+//Бургер
 let menuBtn = document.querySelector('.menu-btn');
 let menu = document.querySelector('.menu');
-
 menuBtn.addEventListener('click', function(){
 	menuBtn.classList.toggle('active');
 	menu.classList.toggle('active');
 })
 
-
-
+// лисенеры
+document.querySelectorAll('.menu li').forEach( btn => {
+  btn.addEventListener('click', setBurgerA)
+})
+// Функции
+function setBurgerA() {
+  let userBtn = event.target
+  if (userBtn.innerHTML == 'Галерея работ') {
+    document.querySelector('.menu').classList.remove('active')
+}}
