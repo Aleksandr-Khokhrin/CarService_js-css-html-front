9d94a2aec66afe383924c44fcf794d7e2f291cab
Aleksandr <125996874+MrAlex-Neo@users.noreply.github.com>
2023-03-22 12:15:50 +0500

    Update scriot.js

--- js/scriot.js
+++ js/scriot.js
@@ -2,10 +2,12 @@
 let menuBtn = document.querySelector('.menu-btn');
 let menu = document.querySelector('.menu');
 
+
 menuBtn.addEventListener('click', function(){
 	menuBtn.classList.toggle('active');
 	menu.classList.toggle('active');
 })
+
 
 //карусель1
 let splideF = new Splide( '.splide.first', {
@@ -40,9 +42,16 @@ splideT.mount();
 document.querySelectorAll('.OurServicesButton').forEach( btn => {
   btn.addEventListener('click', setPoints)
 })
-
-
+document.querySelectorAll('.menu li').forEach( btn => {
+  btn.addEventListener('click', setBurgerA)
+})
 // Функции
+function setBurgerA() {
+  let userBtn = event.target
+  if (userBtn.innerHTML == 'Галерея работ') {
+    document.querySelector('.menu').classList.remove('active')
+}}
+
 function setPoints() {
   let userBtn = event.target
   if (userBtn.innerHTML == 'Оклейка') {
